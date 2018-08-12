8421f8019b4be7879e35972e2a498435ebdc7bd0
last6882@gmail.com <last6882@gmail.com>
2018-08-12 20:55:41 +0600

    five new file added

---
+++ connection.php
@@ -0,0 +1,14 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,"tutorial1");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+?> 
