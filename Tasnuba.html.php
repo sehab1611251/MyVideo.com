2c54d009898e74d9d836d51ec70b3b36db529e82
last6882@gmail.com <last6882@gmail.com>
2018-09-11 22:48:32 +0600

    all old file deleted for adding final files

--- Tasnuba.html.php
+++
@@ -1,2 +0,0 @@
- <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
--- ananya.html.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-   <head>
-<body bgcolor= #02FB02>
-
-     <title>MyVideo.Com</title>
-      <link rel="stylesheet" type="text/css" href="style.css"/>
